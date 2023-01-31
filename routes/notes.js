da6ac409a9fe3d23aeaeced571f0b8d66d546af4
fycn <fannychen623@gmail.com>
2023-01-31 12:15:07 -0500

    completed challenge

---
+++ routes/notes.js
@@ -0,0 +1,55 @@
+const nts = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the feedback
+nts.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// DELETE Route for a specific note
+nts.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  console.log(noteId);
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((title) => title.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});  
+
+// POST Route for submitting feedback
+nts.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
+
+module.exports = nts;
